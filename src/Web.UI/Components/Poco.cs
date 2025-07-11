1e21daf3e8653dd18133927b564ff4c42c65b5b7
Versile Johnson II <versilej@gmail.com>
2025-07-11 10:40:53 -0500

    duplicate mudtry

---
+++ src/Web.UI/Components/Poco.cs
@@ -0,0 +1,9 @@
+namespace Web.UI.Components
+{
+    public class Poco
+    {
+        public string? Prop1 { get; set; }
+        public string? Prop2 { get; set; }
+        public string? Prop3 { get; set; }
+    }
+}
